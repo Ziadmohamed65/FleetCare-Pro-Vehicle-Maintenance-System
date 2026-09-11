8e1319f144ef8277b2b27c24e264957f2befd33d
ZIAD MOHAMED <ziadmohamedhosney@gmail.com>
2026-09-11 22:16:13 +0300

    Add files via upload

---
+++ FleetCare Pro/FleetCare Pro/ViewComponents/FleetCostSummaryViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using FleetCare_Pro.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FleetCare_Pro.ViewComponents
+{
+   
+   public class FleetCostSummaryViewComponent : ViewComponent
+   {
+       private readonly FleetCareDbContext _context;
+
+       public FleetCostSummaryViewComponent(FleetCareDbContext context)
+       {
+           _context = context;
+       }
+
+       public async Task<IViewComponentResult> InvokeAsync()
+       {
+           var totalMaintenanceCost = await _context.ServiceRecords
+               .SumAsync(sr => (decimal?)sr.TotalCost) ?? 0m;
+
+           return View(totalMaintenanceCost);
+       }
+   }
+
+}
---
+++ FleetCare Pro/FleetCare Pro/ViewComponents/OverdueMaintenanceViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using FleetCare_Pro.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FleetCare_Pro.ViewComponents
+{
+    public class OverdueMaintenanceViewComponent : ViewComponent
+    {
+        private readonly FleetCareDbContext _context;
+
+        public OverdueMaintenanceViewComponent(FleetCareDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var sixMonthsAgo = DateTime.UtcNow.AddMonths(-6);
+
+            // Returns vehicles with NO service records OR where all service records are older than 6 months
+            var vehicles = await _context.Vehicles
+            .Include(v => v.ServiceRecords)
+            .Where(v => !v.ServiceRecords.Any(sr => sr.ServiceDate >= sixMonthsAgo))
+            .ToListAsync();
+
+            return View(vehicles);
+        }
+    }
+}
